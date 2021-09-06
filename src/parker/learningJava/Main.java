39d49ad6cfe9e8391d524921126db19dae481d8d
Christian Parker <christianparker512@gmail.com>
2021-09-05 19:31:21 -0500

    for loops for odd numbers

--- src/parker/learningJava/Main.java
+++ src/parker/learningJava/Main.java
@@ -4,6 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("100,000 at 2% interest =" + calculateInterest(10000,2));
+        System.out.println("100,000 at 3% interest =" + calculateInterest(10000,3));
+        System.out.println("100,000 at 4% interest =" + calculateInterest(10000,4));
+        System.out.println("100,000 at 5% interest =" + calculateInterest(10000,5));
+
+        for (int i = 0; i < 5; i++){
+            System.out.println("Loop " + i + " interest rate");
+        }
+        for (int number =1; number <7; number +=2){
+            System.out.println("The number = " + number);
+        }
     }
     public static double calculateInterest (double amount, double interestRate){
         return (amount * (interestRate/100));
